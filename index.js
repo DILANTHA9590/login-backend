0ca9d20b64f0539a97113fc002e87f11f141df19
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-22 20:07:19 +0530

    import .env and routes routes path and   Set up authentication routes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import connectDB from "./db/coonectDB.js";
+import dotenv from "dotenv"; // Load environment variables from .env file
+import authRoutes from "./routes/auth.routes.js";
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
